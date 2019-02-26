7daef213f87105c6ea2ec0980f072da058826aef
andreicicedea <andreicicedea@gmail.com>
2019-02-26 13:10:11 +0200

    minor

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,13 +12,12 @@ class Quiz extends Component {
         }
     }
     render() {
-        const isQuizEnd = ((this.state.quiz_positon -1) === 
+        const isQuizEnd = (this.state.quiz_positon -1 === 
         quizData.quiz_questions.length)
         return (
             <div>
                 {isQuizEnd ? <QuizEnd /> :
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> 
-                }
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> }
             </div>
         )
     }
