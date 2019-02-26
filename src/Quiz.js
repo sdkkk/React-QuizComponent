fc4b4dbf9ff1f45f71bbb0f2167bd3b8b0e43e0b
andreicicedea <andreicicedea@gmail.com>
2019-02-26 13:32:03 +0200

    fixing names

--- src/Quiz.js
+++ src/Quiz.js
@@ -27,7 +27,7 @@ class Quiz extends Component {
         quizData.quiz_questions.length)
         return (
             <div>
-                {isQuizEnd ? <QuizEnd resetCickHandler={this.handleResetClick.bind(this)}/> :
+                {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> :
                     <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/> }
             </div>
         )
